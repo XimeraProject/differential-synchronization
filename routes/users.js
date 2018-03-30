4cbcddfb2391b37ef63fabc3a3b8fa00dc8f9f27
Jim Fowler <fowler@math.osu.edu>
2018-03-30 09:42:37 -0400

    fake data

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    // Some fake data
+  res.json([{
+  	id: 1,
+  	username: "fake-one"
+  }, {
+  	id: 2,
+  	username: "fake-two"
+  }]);
 });
 
 module.exports = router;
